140ea0c64e0a4c7446a974da89ab788670840149
Jean-Olivier Irisson <irisson@normalesup.org>
2016-06-10 13:40:27 +0200

    Complete overhaul of nc_slice (becomes ncvar_slice)
    Use actual values rather than indexes for the extraction
    Many new methods for plotting, converting etc.

--- R/nc_slice.R
+++ R/nc_slice.R
@@ -1,149 +1,220 @@
-#'  Cut a "slice" of a netCDF variable (usually 2D)
+#' Read a slice from a netCDF file
 #'
-#'  @param nc netCDF handle, from \code{\link[ncdf4]{nc_open}}
-#'  @param varname variable name as a character string
-#'  @param ... indexes to extract for each dimension; either named arguments, in which case the unspecified dimensions are fully extracted; or unnamed arguments, in which case dimensions are assumed to be in the same order as in the netCDF variable and NA elements are fully extracted
+#' Reads an array of data (a slice) from an existing netCDF file. Typically this array is two-dimensional and meant to be plotted.
 #'
+#' @param nc netCDF handle, from \code{\link[ncdf4]{nc_open}}
+#' @param varid the variable to read the data from. Needs to be an actual variable, not a dimension variable. Can be a string with the name of the variable or an object of class \code{ncvar4}. If left unspecified, the function will determine if there is only one variable in the file and, if so, read from that.
+#' @param ... ranges of the values to extract, for each dimension. Each range is a vector of numbers, in the unit of the corresponding dimension, and everything between the minimum and maximum of these numbers will be extracted. Ranges can be specified either as named arguments, whose name matches one of the dimensions, or are assumed to be in the same order as the dimensions of the netCDF variable. Unspecified or NA elements are fully extracted.
+#'
+#' @return A list containing vectors for each dimension (of length > 1) and one array containing the extracted data. For two-dimensional slices, this list is suitable for plotting functions such as \code{\link[graphics]{image}}, \code{\link[graphics]{contour}} and \code{\link[graphics]{persp}} after changing the names to "x", "y", and "z". Convenience functions are provided to make this conversion, convert the slice into a data.frame, or plot the slice directly.
+#'
+#' @seealso \code{\link{as.xyz.ncslice4}} and \code{\link{as.data.frame.ncslice4}} to convert a slice to various usual formats; \code{\link{plot.ncslice4}} to plot a slice.
+#' 
 #' @export
 #'
-#' @importFrom ncdf4 ncvar_get
-nc_slice <- function(nc, varname, ...) {
-
+#' @examples
+#' nc <- nc_open(system.file("extdata", "sresa1b_ncar_ccsm3.nc", package="ncdf4helpers"))
+#' # extract everything
+#' x <- ncvar_slice(nc, "tas")
+#' # inspect the result
+#' str(x)
+#' # extract some specific ranges
+#' x <- ncvar_slice(nc, "tas", lon=c(100, 200))
+#' x <- ncvar_slice(nc, "tas", lat=10:80)
+#' x <- ncvar_slice(nc, "tas", lon=c(0,300), lat=c(10:20))
+#' x <- ncvar_slice(nc, "tas", lon=c(0,50), lat=NA)
+#' nc_close(nc)
+#'
+#' # plot the result
+#' image(as.xyz(x))
+#' # or simply
+#' image(x)
+#'
+#' # convert the slice into a data.frame for later manipulation
+#' head(as.data.frame(x))
+ncvar_slice <- function(nc, varid, ...) {
   # check arguments
-  if ( ! "ncdf4" %in% class(nc) ) {
-    stop("nc needs to be a ncdf4 handle, created with nc_open() from package ncdf4")
-  }
-  varname <- match.arg(varname, names(nc$var))
+  # NB: this checks for the class of nc
+  #     this uses an internal, not exported, function in ncdf4 and may therefore be fragile
+  var <- ncdf4:::vobjtovarid4(nc, varid)
+  var_name <- names(nc$var)[var$list_index]
+  
+  # prepare the extraction list
+  # = a list with as many elements as the variables has dimensions, in the order of the variables dimensions and containing min and max of the values to extract for each dimension
 
-  # get dimensions of this variable
-  dims <- nc$var[[varname]]$dim
-  dimNames <- sapply(dims, function(x) {x$name})
-  names(dims) <- dimNames   # makes it easier to extract them
-
-  # detect whether data is extracted based on dimension *names* or positions and construct the extraction list appropriately
-  # the extraction list is a list of indexes to extract, one element for each dimension of the chosen variable
+  # initialise the extraction list
   extract <- list(...)
-  extractNames <- names(extract)
+  
+  # get dimensions of this variable
+  dims <- nc$var[[var$list_index]]$dim
+  dim_names <- sapply(dims, function(x) {x$name})
+  names(dims) <- dim_names   # makes it easier to extract the values later
 
-  if ( ! is.null(extractNames) ) {
-    # Use the names
+  # check the number of extracted dimensions
+  if (length(extract) > length(dims)) {
+    stop("You specified ", length(extract)," ranges to extract but `", var_name, "` has only ", length(dims), " dimensions: ", paste(dim_names, collapse=", "), ".")
+  }
 
-    # check that all names are actually dimension names
-    if ( ! all(extractNames %in% dimNames) ) {
-      notOK <- extractNames[! extractNames %in% dimNames ]
-      if (length(notOK) > 1) {
-        notOK <- paste(notOK, collapse=", ")
-        mess <- " are not dimensions of "
-      } else {
-        mess <- " is not a dimension of "
-      }
-      stop(notOK, mess, varname)
-    }
-
-    # detect matching names
-    matching <- match(dimNames, extractNames)
-
-    # create the extraction list with NA when the dimension is not specified
-    extract <- extract[matching]
-    extract[is.na(matching)] <- NA  # by default these are NULL
-
+  # fill missing names assuming the variables are in order
+  if (is.null(names(extract))) {
+    # all missing names
+    names(extract) <- dim_names[seq(along=extract)]
+    # NB: using seq(along=...) here allows to deal with empty ...
   } else {
-    # Consider that the list contains all the dimensions in the correct order (or is empty entirely)
-    # check that these conditions are satisfied
-    
-    if ( length(extract) == 0 ) {
-
-      # if the list is empty, just put NAs everywhere: we extract everything
-      extract <- as.list(rep(NA, times=length(dims)))
-                  
-    } else if ( length(extract) != length(dims) ) {
-      
-      # the extraction arguments provided are not appropriate
-      stop("You specified ", length(extract)," dimensions but variable", varname, " has ", length(dims), " dimensions:", paste(dimNames, collapse=", "), ".")
-
-    }
+    # some missing names
+    missing_names <- names(extract) == ""
+    names(extract)[missing_names] <- dim_names[missing_names]
   }
   
-  # make sure the extraction list has appropriate names
-  names(extract) <- dimNames
-  # TODO check wether that is useful
-
-  # replace NA dimensions with all indexes for the corresponding dimensions
-  for ( i in 1:length(dims) ) {
-    if ( all(is.na(extract[[i]])) ) {
-      extract[[i]] <- 1:(dims[[i]]$len)
-    }
+  # check that provided names are OK
+  invalid_names <- setdiff(names(extract), dim_names)
+  if (length(invalid_names) != 0) {
+    stop(paste(invalid_names, collapse=", "), " not among valid dimension names. Should be one of: ", paste(dim_names, collapse=", "))
   }
   
-  # compute start and count vectors for ncvar_get
-  start <- sapply(extract,min)
-  stop  <- sapply(extract,max)
-  count <- stop - start+1
+  # reorder dimensions and add missing dimensions
+  extract <- extract[dim_names]
+  names(extract) <- dim_names
   
-  # extract slice
-  slice <- ncvar_get(nc, varname, start=start, count=count)
-  
-  # detect which dimensions have an actual dimension in the extracted array i.e. those with a count > 1
-  extractedDimNames <- names(count)[which(count > 1)]
-
-  # get the values of the coordinates extracted in these dimensions
-  coords <- lapply(extractedDimNames, function(x) {
-    dims[[x]]$vals[extract[[x]]]
+  # compute the extracted range for each dimension
+  # this also replaces NA or NULL dimensions with a full range ]-Inf,+Inf[
+  extract <- lapply(extract, function(x) {
+    suppressWarnings(sort(range(x, na.rm=T)))
   })
-  names(coords) <- extractedDimNames
   
-  # give the slice dimension names, which makes it easier to melt/plot
-  dimnames(slice) <- coords
+  # for each dimension, get the coordinates of the extracted points and compute start and count for ncvar_get
+  for (dim in dim_names) {
+    # get the range and values of the dimensions
+    r <- extract[[dim]]
+    vals <- nc$dim[[dim]]$vals
+    # compute the indexes inside the range
+    idx <- which(vals >= r[1] & vals <= r[2])
+    # extract the coordinates, start, stop and compute count
+    vals <- vals[idx]
+    start <- min(idx)
+    stop  <- max(idx)
+    count <- stop - start + 1
+    # store result as a list
+    extract[[dim]] <- list(vals=vals, start=start, count=count)
+  }
+
+  # prepare the arguments for ncvar_get
+  start <- sapply(extract, `[[`, "start")
+  count <- sapply(extract, `[[`, "count")
+
+  # extract the data
+  dat <- ncdf4::ncvar_get(nc, varid, start=start, count=count)
+  
+  # store coordinates and data in a list (such as the ones used for image, persp, etc.)
+  coords <- lapply(extract, `[[`, "vals")
+  slice <- coords[count > 1] # eliminate coordinates with one extracted element
+  slice[[var_name]] <- dat
   
   # give it a specific class to make clever stuff with it afterwards
-  class(slice) <- c("nc_slice", class(slice))
-  
-  # add the variable name as attribute
-  attr(slice, "varname") <- varname
+  class(slice) <- c("ncslice4", class(slice))
   
   return(slice)
 }
 
-# as.xyz.nc_slice <- function(x) {
-#   coords <- dimnames(x)
-# }
 
-#'  Plot a "slice" extracted from a NetCDF file
+#' Coerce to a x,y,z list
+#' 
+#' Generic function to convert an object into an x,y,z list suitable for \code{\link[graphics]{image}}, \code{\link[graphics]{contour}}, \code{\link[graphics]{persp}}, etc.
 #'
-#'  @param x a NetCDF slice object, extracted via \code{\link{nc_slice}}
-#'  @param ... passed to \code{image}
+#' @param x an R object
 #'
 #' @export
-#' @seealso \code{\link{nc_slice}}
-plot.nc_slice <- function(x, ...) {
-  
-  if ( length(dim(x)) != 2 ) {
-    stop("Can only plot a slice with 2 dimensions")
+as.xyz <- function(x) UseMethod("as.xyz", x)
+
+#' Convert a NetCDF slice into xyz list
+#'
+#' Convert a slice read from a NetCDF file with \code{\link{ncvar_slice}} into a x,y,z list suitable for \code{\link[graphics]{image}}, \code{\link[graphics]{contour}}, \code{\link[graphics]{persp}}, etc.
+#'
+#' @param x a NetCDF slice object, extracted via \code{\link{ncvar_slice}}
+#'
+#' @seealso \code{\link{ncvar_slice}} to read the slice and \code{\link{as.data.frame.ncslice4}} to convert it into another format.
+#'
+#' @export
+#'
+#' @examples
+#' nc <- nc_open(system.file("extdata", "sresa1b_ncar_ccsm3.nc", package="ncdf4helpers"))
+#' x <- ncvar_slice(nc, "tas")
+#' nc_close(nc)
+#' # inspect and transform the result
+#' str(x)
+#' x <- as.xyz(x)
+#' str(x)
+as.xyz.ncslice4 <- function(x) {
+  if (length(x) > 3) {
+    stop("Can only handle a 2-dimensional slice. This one has ", length(x)-1)
   }
-  
-  # make a list for image/persp/...
-  out <- dimnames(x)
-  out <- lapply(out, as.numeric)
-  names(out) <- c("x", "y")
-  out$z <- x
-
-  # plot an image
-  image(out, ...)
-
-  return(invisible(out))
+  # change the names for persp and the like
+  names(x) <- c("x", "y", "z")
+  # remove the class to avoid infinite recursion
+  x <- unclass(x) 
+  return(x)
 }
 
-#'  Convert a "slice" extracted from a NetCDF file into a data.frame
+
+#' Convert a NetCDF slice into a data.frame
 #'
-#'  @param x a NetCDF slice object, extracted via \code{\link{nc_slice}}
-#'  @param ... passed to generic method
+#' Convert a slice read from a NetCDF file with \code{\link{ncvar_slice}} into a data.frame for further manipulation of the data.
+#'
+#' @param x a NetCDF slice object, extracted via \code{\link{ncvar_slice}}.
+#' @param row.names \code{NULL} or a character vector giving the row names for the data frame.  Missing values are not allowed.
+#' @param optional logical. If \code{TRUE}, setting row names and converting column names (to syntactic names: see \code{\link[base]{make.names}}) is optional.
+#' @param ... ignored.
+#'
+#' @seealso \code{\link{ncvar_slice}} to read the slice and \code{\link{as.xyz.ncslice4}} to convert it into another format.
 #'
 #' @export
-#' @seealso \code{\link{nc_slice}}
-#' @importFrom reshape2 melt
-as.data.frame.nc_slice <- function(x, ...) {
-  out <- melt(x, value.name=attr(x, "varname"))
-  out <- as.data.frame(out, ...)
-  return(out)
+#' @import reshape2
+#'
+#' @examples
+#' nc <- nc_open(system.file("extdata", "sresa1b_ncar_ccsm3.nc", package="ncdf4helpers"))
+#' x <- ncvar_slice(nc, "tas")
+#' nc_close(nc)
+#' head(as.data.frame(x))
+as.data.frame.ncslice4 <- function(x, row.names=NULL, optional=FALSE, ...) {
+  n <- length(x)
+  # convert the data
+  d <- reshape2::melt(x[[n]])
+  # replace indexes by actual values for each dimension
+  for (i in 1:(n-1)) {
+    d[,i] <- x[[i]][d[,i]]
+  }
+  names(d) <- names(x)
+  # handle as.data.frame generic arguments
+  d <- as.data.frame(d, row.names=row.names, optional=optional)
+  return(d)
 }
 
+
+#' Plot a slice extracted from a NetCDF file
+#'
+#' @param x a NetCDF slice object, extracted via \code{\link{ncvar_slice}}
+#' @param ... passed to the plotting function
+#'
+#' @seealso \code{\link{ncvar_slice}}
+#'
+#' @examples
+#' nc <- nc_open(system.file("extdata", "sresa1b_ncar_ccsm3.nc", package="ncdf4helpers"))
+#' x <- ncvar_slice(nc, "tas", lon=10:30, lat=30:50)
+#' nc_close(nc)
+#' image(x)
+#' contour(x, add=TRUE)
+#' persp(x, theta=210, phi=30)
+#'
+#' @export
+#' @import graphics
+#' @rdname plot.ncslice4
+image.ncslice4 <- function(x, ...) { graphics::image(as.xyz(x), ...) }
+#' @export
+#' @rdname plot.ncslice4
+plot.ncslice4 <- image.ncslice4
+#' @export
+#' @rdname plot.ncslice4
+contour.ncslice4 <- function(x, ...) { graphics::contour(as.xyz(x), ...) }
+#' @export
+#' @rdname plot.ncslice4
+persp.ncslice4 <- function(x, ...) { graphics::persp(as.xyz(x), ...) }
